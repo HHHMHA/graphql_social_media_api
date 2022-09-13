0ea156071aa514b437a42fd4776b0841fc50f0b4
J2mF <mp4-12cs5@hotmail.com>
2022-09-13 18:14:22 +0300

    Setup graphql

--- graphql_social_media_api/settings.py
+++ graphql_social_media_api/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # 3rd
+    'graphene_django',
+
+    # local
+    'graphql_api',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+GRAPHENE = {
+    "SCHEMA": "graphql_social_media_api.schema.schema"
+}
